b06be412ba5aeeaa067e28b5860a4d6c8bb95ab7
gvvreddy09 <150003158+gvvreddy09@users.noreply.github.com>
2023-11-06 13:32:16 +0530

    Code Alignment

--- Quarterly Expense Summary.sql
+++ Quarterly Expense Summary.sql
@@ -2,8 +2,8 @@
 CREATE TABLE expenses
 (	date DATE,
 	description VARCHAR(50),
-    category VARCHAR(15),
-    amount INT
+    	category VARCHAR(15),
+    	amount INT
 );
 
 -- Inseting records to Expenses
@@ -30,27 +30,27 @@ VALUES ('2022-01-01','Saravana bhavan','Food',2700),
 ('2022-10-18', 'Verizon wireless', 'Utilities', '3000'),
 ('2022-11-19', 'Banglore muni water bill', 'Utilities', '400');
 
+-- Problem Statement Solution
+WITH pivot AS (
 
-with pivot as (
-
-with quarter_expenses as (
+WITH quarter_expenses AS (
 	SELECT *,
         CASE
-			when month(date) in (1,2,3) then "Q1"
-            when month(date) in (4,5,6) then "Q2"
-            when month(date) in (7,8,9) then "Q3"
-            else "Q4"
-        END as quarter
+	    WHEN MONTH(date) IN (1,2,3) THEN "Q1"
+            WHEN MONTH(date) IN (4,5,6) THEN "Q2"
+            WHEN MONTH(date) IN (7,8,9) THEN "Q3"
+            ELSE "Q4"
+        END AS quarter
 	FROM expenses	)
     
-select quarter, category, sum(amount) as amount from quarter_expenses
+SELECT quarter, category, sum(amount) AS amount FROM quarter_expenses
 GROUP BY quarter, category	)
 
-select 
-		category,
-        Max(CASE WHEN quarter="Q1" Then amount	END) as Q1,
-        Max(CASE WHEN quarter="Q2" Then amount END) as Q2,
-        Max(CASE WHEN quarter="Q3" Then amount END) as Q3,
-        Max(CASE WHEN quarter="Q4" Then amount END) as Q4
-from pivot
-group by category;
+SELECT 
+	category,
+        Max(CASE WHEN quarter="Q1" Then amount	END) AS Q1,
+        Max(CASE WHEN quarter="Q2" Then amount END) AS Q2,
+        Max(CASE WHEN quarter="Q3" Then amount END) AS Q3,
+        Max(CASE WHEN quarter="Q4" Then amount END) AS Q4
+FROM pivot
+GROUP BY category;
